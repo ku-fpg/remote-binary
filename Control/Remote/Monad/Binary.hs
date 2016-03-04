8737d2441f8d39179c9b60fe9a4b3082ab92c94c
jtdawso <jdawson@ku.edu>
2016-03-04 16:12:54 -0600

    Added Haddock cleaned up unused functions

--- Control/Remote/Monad/Binary.hs
+++ Control/Remote/Monad/Binary.hs
@@ -14,14 +14,9 @@ Portability: GHC
 -}
 
 module Control.Remote.Monad.Binary
-    ( sendBinaryQ
-    , receiveSendAPI
-    , BinaryQ(..)
+    ( BinaryQ(..)
     , Fmap(..)
     , SendAPI(..)
-    , weakSession
-    , strongSession
-    , applicativeSession
     , send
     , monadClient
     , server
@@ -29,41 +24,18 @@ module Control.Remote.Monad.Binary
 import           Control.Natural
 import           Control.Remote.Monad
 import           Control.Remote.Monad.Binary.Types
-import  qualified Control.Remote.Monad.Packet.Weak as WP
-import  qualified Control.Remote.Monad.Packet.Strong as SP
 import  qualified Control.Remote.Monad.Packet.Applicative as AP
 import           Data.Binary
 import           Data.Binary.Put (runPut)
 import           Data.Binary.Get (runGet)
 
-
+-- Internal sending of BinaryQ object
 sendBinaryQ :: (BinaryQ f) => (SendAPI ~> IO) -> f a -> IO a
 sendBinaryQ f pkt = do 
                         r <- f (Sync (encodePacket  pkt))
                         return $ decodePacketResult pkt r 
-                 
-
-
-receiveSendAPI :: (BinaryQ f) => (f :~> IO) -> (SendAPI ~> IO)
-receiveSendAPI (Nat f) (Sync c) = do 
-                     case runGet getQ c of 
-                       (Fmap f' v) -> do 
-                                  r  <- f v 
-                                  return $ runPut $ f' r
-
-weakSession :: forall c p f . (f ~ WP.WeakPacket, BinaryQ (f c p), RunMonad f) => (WP.WeakPacket c p :~> IO ) -> (RemoteMonad c p  :~> IO)
-weakSession  f =  runMonad g
-           where
-                g :: (WP.WeakPacket c p  :~> IO)
-                g = nat $ sendBinaryQ  $ receiveSendAPI f
-
-strongSession :: (BinaryQ p, Binary c) => (SP.StrongPacket c p :~>IO) -> (RemoteMonad c p  :~> IO)
-strongSession  f =  runMonad f
-
-applicativeSession :: (BinaryQ p, Binary c) => (AP.ApplicativePacket c p :~>IO) -> (RemoteMonad c p  :~> IO)
-applicativeSession  f =  runMonad f
-
-
+-- | This function is used to convert a function that can transport ByteStrings in a SendAPI wrapper to a function
+--   that can use the remote-monad bundling strategies and then send them via the input function.
 monadClient :: forall c p f . (f ~ AP.ApplicativePacket, BinaryQ (f c p))=> (SendAPI ~> IO) -> (RemoteMonad c p  :~> IO)
 monadClient f = runMonad g
           where
@@ -71,11 +43,20 @@ monadClient f = runMonad g
                g = nat $ sendBinaryQ f
 
 
+-- internal receiving of sync object
+receiveSendAPI :: (BinaryQ f) => (f :~> IO) -> (SendAPI ~> IO)
+receiveSendAPI (Nat f) (Sync c) = do 
+                     case runGet getQ c of 
+                       (Fmap f' v) -> do 
+                                  r  <- f v 
+                                  return $ runPut $ f' r
+
+-- | This function takes a function that can execute a remote-monad packet containing the User's GADT 
+--   and elevates it to handle the encoding of the response
 server :: (Binary c, BinaryQ p)=> (AP.ApplicativePacket c p :~> IO ) -> (SendAPI :~> IO )
 server f =  nat $ receiveSendAPI $ f
 
-
-
+-- | send remote monad , equivalent to executing the natural transformation on the RemoteMonad
 send :: (RemoteMonad c p :~> IO) -> (RemoteMonad c p a)-> IO a
 send f m =  f # m 
 
